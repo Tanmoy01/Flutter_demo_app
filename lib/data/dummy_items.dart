e7ba71e58c1cc14ec803b9f722be5244a018f8a9
Tanmoy01 <tanmoy.barui.01@gmail.com>
2023-08-16 23:11:27 +0530

    first commit

---
+++ lib/data/dummy_items.dart
@@ -0,0 +1,21 @@
+import 'package:test_demo/models/grocery_item.dart';
+import 'package:test_demo/data/categories.dart';
+import 'package:test_demo/models/categories.dart';
+
+final groceryItems = [
+  GroceryItem(
+      id: 'a',
+      name: 'Milk',
+      quantity: 1,
+      category: categories[Categories.dairy]!),
+  GroceryItem(
+      id: 'b',
+      name: 'Bananas',
+      quantity: 5,
+      category: categories[Categories.fruit]!),
+  GroceryItem(
+      id: 'c',
+      name: 'Beef Steak',
+      quantity: 1,
+      category: categories[Categories.meat]!),
+];
